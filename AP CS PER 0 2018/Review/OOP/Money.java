b10f5b71dc6732410d444729345641d23db018ec
19vtovmasian <19vtovmasian@31-LAB1139-18.busd.local>
2018-10-18 07:08:31 -0700

    hoohoo

--- AP CS PER 0 2018/Review/OOP/Money.java
+++ AP CS PER 0 2018/Review/OOP/Money.java
@@ -7,39 +7,86 @@ public class Money {
 		
 	// constructors
 	public Money() {
-
+		myDollars = 0;
+		myCents = 0;
 	}
 
 	public Money(int dollars, int cents) { 
+		if(cents > 99) {
+			dollars += (cents / 100);
+			cents %= 100;
+		}
+		
+		this.myDollars = dollars;
+		this.myCents = cents;
+		
 
+		
 	}
 
 	// methods 
 	// preconditions: Money object has been constructed //
 	// postconditions: returns the value held in myDollars
 	public int getDollars() {
-	
+		return myDollars;
 	}
 
 	// preconditions: Money object has been constructed // postconditions: returns
 	// the value held in myCents
 	public int getCents() {
-		
+		return myCents;
 	}
 	
 	// preconditions: Money object has been constructed //
 	// postconditions: returns the Money object as a String // Also consider if
 	// myCents is a one digit value
 	public String toString() {
-		
+		return (myCents > 9) ? "$" + myDollars + "." + myCents : "$" + myDollars + ".0" + myCents;
 	}
 
 	// preconditions: Money object has been constructed // postconditions: prints
 	// the value of the Money object // using the fewest number of bills and //
 	// coins possible.
 	public void getAndPrintDetails() {
-		int hundreds, fifties, twenties, tens, fives, ones;
-		int quarters, dimes, nickels, pennies;
+		int hundreds = 0, fifties = 0, twenties = 0, tens = 0, fives = 0, ones = 0;
+		int quarters = 0, dimes = 0, nickels = 0, pennies = 0;
+		
+		int dollars = myDollars;
+		int cents = myCents;
+		
+		
+		if(dollars >= 5 || cents >= 5) {
+			hundreds += dollars / 100;
+			dollars %= 100;
+			
+			fifties += dollars / 50;
+			dollars %= 50;
+			
+			twenties += dollars / 20;
+			dollars %= 20;
+			
+			tens += dollars / 10;
+			dollars %= 10;
+			
+			fives += dollars / 5;
+			dollars %= 5;
+			
+			ones = dollars;
+		
+			quarters += cents / 25;
+			cents %= 25;
+			
+			dimes += cents / 10;
+			cents %= 10;
+			
+			nickels += cents / 5;
+			cents %= 5;
+			
+			pennies = cents;
+		}
+		
+		
+		
 		System.out.println("\t" + hundreds + " hundreds");
 		System.out.println("\t" + fifties + " fifties");
 		System.out.println("\t" + twenties + " twenties");
@@ -60,6 +107,7 @@ public class Money {
 	// neither?
 	public Money add(int dollars, int cents) {
 		
+		return new Money(myDollars + dollars, myCents + cents);
 	}
 
 	// preconditions: Money object has been constructed // postconditions: returns
@@ -67,5 +115,6 @@ public class Money {
 	//
 	public Money add(Money theMoney) {
 		
+		return add(theMoney.getDollars(), theMoney.getCents());
 	}
 }
